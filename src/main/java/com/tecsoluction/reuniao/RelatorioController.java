9c8da433b8b44b0069be2c7da1bdd758926685cb
cleberson <clebrsonn@hotmail.com>
2017-02-11 18:56:07 -0300

    colocando lombok

--- src/main/java/com/tecsoluction/reuniao/RelatorioController.java
+++ src/main/java/com/tecsoluction/reuniao/RelatorioController.java
@@ -1,22 +1,14 @@
 package com.tecsoluction.reuniao;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import com.tecsoluction.reuniao.framework.AbstractController;
-import com.tecsoluction.reuniao.framework.AbstractEntityDao;
-import com.tecsoluction.reuniao.dao.VereadorDAO;
-import com.tecsoluction.reuniao.entidade.Usuario;
-import com.tecsoluction.reuniao.entidade.Vereador;
+
+import java.util.Locale;
 
 /**
  * Handles requests for the application home page.
