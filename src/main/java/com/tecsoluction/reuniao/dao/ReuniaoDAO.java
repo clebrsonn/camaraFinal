ce2e72c7695e3a32e3b860a79898d6d96e63182c
cleberson <clebrsonn@hotmail.com>
2017-02-11 22:11:56 -0300

    jpa dao

--- src/main/java/com/tecsoluction/reuniao/dao/ReuniaoDAO.java
+++ src/main/java/com/tecsoluction/reuniao/dao/ReuniaoDAO.java
@@ -1,13 +1,13 @@
 package com.tecsoluction.reuniao.dao;
 
 import com.tecsoluction.reuniao.entidade.Reuniao;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by clebr on 11/02/2017.
  */
 @Repository
-public interface ReuniaoDAO extends CrudRepository<Reuniao, Long> {
+public interface ReuniaoDAO extends JpaRepository<Reuniao, Long> {
 
 }
--- src/main/java/com/tecsoluction/reuniao/dao/UsuarioDAO.java
+++ src/main/java/com/tecsoluction/reuniao/dao/UsuarioDAO.java
@@ -1,12 +1,12 @@
 package com.tecsoluction.reuniao.dao;
 
 import com.tecsoluction.reuniao.entidade.Usuario;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by clebr on 11/02/2017.
  */
 @Repository
-public interface UsuarioDAO extends CrudRepository<Usuario, Long> {
+public interface UsuarioDAO extends JpaRepository<Usuario, Long> {
 }
